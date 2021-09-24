f7f5531ef53bd3d73f6df3e618eb572b106ee8b0
elliot.loftus <ejloftus@gmail.com>
2021-09-24 14:12:15 -0600

    first commit

---
+++ src/main/kotlin/message/MessageeApplication.kt
@@ -0,0 +1,11 @@
+ package message
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MessageeApplication
+
+fun main(args: Array<String>) {
+    runApplication<MessageeApplication>(*args)
+}
