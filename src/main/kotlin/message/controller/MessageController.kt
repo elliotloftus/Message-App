0480762086fa7af6d29fd5f5ccb1d1ef4f97f550
elliot.loftus <ejloftus@gmail.com>
2021-09-25 11:34:31 -0600

    stub out all controller endpoints

--- src/main/kotlin/message/controller/MessageController.kt
+++ src/main/kotlin/message/controller/MessageController.kt
@@ -1,10 +1,7 @@
 package message.controller
 
 import message.model.Message
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class MessageController {
@@ -14,9 +11,30 @@ class MessageController {
         return "Hello"
     }
 
-    @PostMapping("send-message")
+    @PostMapping("/message")
     fun sendMessage(@RequestBody message: Message): Message {
         return message
     }
+
+    @GetMapping("/message/sender/{senderId}/recipient/{recipientId}")
+    fun getMessageBySenderForRecipient(
+        @PathVariable senderId: String,
+        @PathVariable recipientId: String
+    ) : List<Message> {
+        return listOf(Message(senderId,recipientId,"holdere"))
+    }
+
+    @GetMapping("/message/sender/{senderId}")
+    fun getMessageBySender(
+        @PathVariable senderId: String,
+    ) : List<Message> {
+        return listOf(Message(senderId,"recipient","holdere"))
+    }
+
+    @GetMapping("/message")
+    fun getAllMessages(): List<Message>{
+        return listOf(Message("senderId","recipient","holdere"))
+    }
+
 
 }
