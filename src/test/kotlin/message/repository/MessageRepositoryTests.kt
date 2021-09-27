805ad553f5ab96651c03730adada688e9a76f688
elliot.loftus <ejloftus@gmail.com>
2021-09-27 10:51:33 -0600

    start tests

---
+++ src/test/kotlin/message/repository/MessageRepositoryTests.kt
@@ -0,0 +1,13 @@
+package message.repository
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+
+class MessageRepositoryTests {
+        fun todo() {
+            print("hold")
+        }
+
+}
